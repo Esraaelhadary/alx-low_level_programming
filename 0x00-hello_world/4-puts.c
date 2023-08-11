2ecb294f8322f755572209d571633a61aebca8df
Esraa elhadary <esraaelhadary@gmail.com>
2023-08-11 06:06:40 -0700

    g

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,8 @@
 /*first one*/
 int main(void)
 {
-const char *message = "\"Programming is like building a multilingual puzzle";    
-puts(message);    
+const char *message = \"Programming is like building a multilingual"
+"puzzle";
+puts(message);
 return (0);
 }
