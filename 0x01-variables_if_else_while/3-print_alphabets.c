aa152e4f6b820f51b3cd64d66ddc912734134ca9
Esraa elhadary <esraaelhadary@gmail.com>
2023-08-12 16:21:02 -0700

    f

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
  * main - entry point
- * Description: prints the alphabet in lowercase
+ * Description: prints the alphabet in lowercase & uppercase
  * Return: 0 (Success)
  */
 int main(void)
 {
 char letter = 'a';
 char capital = 'A';
-while (letter <= 'z' $$ capital <= 'Z')
+while ((letter <= 'z') $$ (capital <= 'Z'))
 {
 putchar(letter);
 putchar(capital);
