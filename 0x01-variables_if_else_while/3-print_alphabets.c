c243bac79d61a7512f764f500ca2c0206bbc192f
Esraa elhadary <esraaelhadary@gmail.com>
2023-08-12 16:22:17 -0700

    g

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,7 +8,7 @@ int main(void)
 {
 char letter = 'a';
 char capital = 'A';
-while ((letter <= 'z') $$ (capital <= 'Z'))
+while ((letter <= 'z') && (capital <= 'Z'))
 {
 putchar(letter);
 putchar(capital);
