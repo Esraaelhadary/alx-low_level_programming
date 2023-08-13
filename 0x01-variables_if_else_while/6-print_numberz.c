cf7c089facfc41d391fa40e9c85c2f3d8752954b
Esraa elhadary <esraaelhadary@gmail.com>
2023-08-13 01:41:01 -0700

    g

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prints all single digit numbers of base 10 starting from 0
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int single = 0;
+while (single <= 9)
+{
+putchar(single);
+single++;
+}
+putchar('\n');
+return (0);
+}
