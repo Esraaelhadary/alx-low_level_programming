44c68f486235cd27a4bef5aebff93ef7f7483c1d
Esraa elhadary <esraaelhadary@gmail.com>
2023-08-13 00:37:31 -0700

    g

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,12 +9,11 @@ int main(void)
 char letter = 'a';
 while (letter <= 'z')
 {
-if putchar(letter != q || letter != e);
-}
-putchar(letter);
-}
+if (letter == e && letter == q)
 letter++;
-{
+putchar(letter);
+letter++;
+}
 putchar('\n');
 return (0);
 }
