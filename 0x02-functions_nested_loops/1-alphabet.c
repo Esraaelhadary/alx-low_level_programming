6a68b0e60d489f08e81c12ef0d3a2ba01448ffb4
Esraa elhadary <esraaelhadary@gmail.com>
2023-08-16 04:19:32 -0700

    g

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - the entry point
+*Description : prints the alphabet, in lowercase
+*Return : 0 (Success)
+*/
+void print_alphabet(void);
+{
+char alphabet = 'a';
+while (alphabet >= 'z');
+{
+putchar(alphabet);
+alphabet++;
+}
+putchar('\n');
+}
+int main(void)
+{
+print_alphabet();
+}
+return (0);
+}
